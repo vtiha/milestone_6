fcda71584b6d6b8f38e8d20a8d91b42a54ede814
vtiha <tytorenko.kateryna@gmail.com>
2024-10-21 20:58:36 +0300

    Add homework file to milestone_1 again

---
+++ equations.py
@@ -0,0 +1,12 @@
+eq = '4x**2 +4x +    (-8) =  0'
+a = float(eq.replace(' ','').split('x**2')[0])
+b = float(eq.replace(' ','').split('x**2')[1].split('x')[0])
+c = float(eq.replace(' ', '').replace('(','').replace(')', '').replace('+','').split('x')[-1].split('=')[0])
+
+print(a, b, c) # 4 4 -8
+
+x1 = (-b+(b**2-4*a*c)**0.5)/(2*a)
+x2 = (-b-(b**2-4*a*c)**0.5)/(2*a)
+
+
+print(x1, x2) # 1 -2
