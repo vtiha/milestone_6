0ccdc7ab4b397fe455201caef1ce33f7d47956ed
vtiha <tytorenko.kateryna@gmail.com>
2024-10-30 22:22:05 +0200

    add file for milestone 7

---
+++ milestone_5/app.py
@@ -0,0 +1,70 @@
+from flask import Flask, request
+import csv
+import json
+from datetime import datetime
+
+app = Flask(__name__)
+
+def load_employees(filename):
+    employees = []
+    with open(filename, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            employees.append({
+                'id': len(employees) + 1,
+                'full_name': row['Full_Name'],
+                'hiring_date': row['Hiring_Date'],
+                'department': row['Department'],
+                'birthday': row['Birthday'],
+                'phone_number': row['Phone_Number']
+            })
+    return employees
+
+employees = load_employees('database.csv')
+
+@app.get('/birthdays')
+def get_birthdays():
+    month = request.args.get('month', '').lower()
+    department = request.args.get('department', '').lower()
+    report = {
+        "employees": [],
+        "total": 0
+    }
+
+    for emp in employees:
+        if emp['department'].lower() == department and datetime.strptime(emp['birthday'], '%Y-%m-%d').strftime('%B').lower() == month:
+            report['employees'].append({
+                "id": emp['id'],
+                "name": emp['full_name'],
+                "birthday": datetime.strptime(emp['birthday'], '%Y-%m-%d').strftime('%b %d')  # Форматування дати
+            })
+    
+    report['total'] = len(report['employees'])
+
+    response = json.dumps(report, indent=4)
+    return response, 200
+
+@app.get('/anniversaries')
+def get_anniversaries():
+    month = request.args.get('month', '').lower()
+    department = request.args.get('department', '').lower()
+    report = {
+        "employees": [],
+        "total": 0
+    }
+
+    for emp in employees:
+        if emp['department'].lower() == department and datetime.strptime(emp['hiring_date'], '%Y-%m-%d').strftime('%B').lower() == month:
+            report['employees'].append({
+                "id": emp['id'],
+                "name": emp['full_name'],
+                "hiring_date": datetime.strptime(emp['hiring_date'], '%Y-%m-%d').strftime('%b %d')  # Форматування дати
+            })
+
+    report['total'] = len(report['employees'])
+
+    response = json.dumps(report, indent=4)
+    return response, 200
+
+if __name__ == '__main__':
+    app.run(port=5000)
